9f31d4f14d25358ac8b5604539ab13ffd8a11f05
edgard <santoos.ed@gmail.com>
2015-05-29 18:42:29 -0300

    criacao model banner

--- bolosdaluapp/models/banner.py
+++ bolosdaluapp/models/banner.py
@@ -14,13 +14,14 @@ POSICAO = ((PRIMEIRO, '1º'), (SEGUNDO, '2º'), (TERCEIRO, '3º'), (QUARTO, '4º
 class Banner(models.Model):
     imagem = ImageField(upload_to='media/images/uploads',
                         blank=False, null=False)
+    legenda = models.CharField(max_length=100, null=False, blank=False)
     posicao = models.CharField(choices=POSICAO, max_length=2, null=False, blank=False)
 
 
-def __unicode__(self):
-    pass
+    def __unicode__(self):
+        return self.legenda
 
 
-class Meta:
-    db_table = 'banner'
-    app_label = 'bolosdaluapp'
+    class Meta:
+        db_table = 'banner'
+        app_label = 'bolosdaluapp'
